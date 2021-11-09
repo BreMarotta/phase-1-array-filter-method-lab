3d70ff4141c9295055fa14ec0c942f053d724f79
Breanne <BreMarotta@gmail.com>
2021-11-08 19:18:20 -0500

    commit tree 50144e3a6b9a96e9319b857da10a376f218923af

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+function findMatching(array, string){
+    const match = array.filter( object => object.toLowerCase() === string.toLowerCase());
+    return match;  
+}
+
+function fuzzyMatch(array, string){
+  //  const begin = array.filter( object => object.indexOf([object], 0) === string);
+     //   if (begin){
+           // return begin;
+        //}
+       // return begin.name;
+    let length = string.length;
+    return array.filter(driver => driver.slice(0, length)=== string);
+}
+
+function matchName(array, string){
+    const same = array.filter( object => object.name.toLowerCase() === string.toLowerCase());
+        return same;
+}
+//name and hometown are 2 known properties for test
